62ed5d367f7e1cd50cfc49a78380e356d835e1ca
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-17 16:12:47 +0000

    1-compiler

--- hello_world/1-main.c
+++ hello_world/1-main.c
@@ -1,8 +1,7 @@
-/**                                                                                                                                                                    
- * main - Entry point                                                                                                                                                  
- *                                                                                                                                                                     
- * Return: Always 0 (Success)                                                                                                                               
-          
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
