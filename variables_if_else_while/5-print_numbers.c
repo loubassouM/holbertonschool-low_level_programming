17d6acf727b82b491534cde02fcaaf78731f01bf
loubassouM <loubassoumurphy@gmail.com>
2025-02-20 13:04:13 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,12 @@
 
 int main(void)
 {
-  
-int num;
-for (num = 0; num <= 9; num++)
-putchar(num + '0');        
-putchar('\n');
-return (0);
-  
-}
+  int num;
+ 
+  for (num = 0; num <= 9; num++)   
+      putchar(num + '0');
+ 
+  putchar('\n');
+ 
+  return(0);
+ }
