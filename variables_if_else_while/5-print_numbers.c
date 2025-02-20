d837860447488f1be8a5d187fa31d66d1b3a6899
loubassouM <loubassoumurphy@gmail.com>
2025-02-20 12:49:28 +0000

    5-print_numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,9 +7,11 @@
 
 int main(void)
 {
+  
 int num;
 for (num = 0; num <= 9; num++)
 putchar(num + '0');        
 putchar('\n');
-return (0);
+return (0)
+  
 }
