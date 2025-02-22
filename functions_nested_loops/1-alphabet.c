ad9363c3545001d1004b07c362493b4ec0c5d72b
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-22 23:14:31 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 #include <unistd.h>
 
+
 void _putchar(char c);
 
 /**
- *
- */
+*
+*
+*
+*/
 void print_alphabet(void)
+
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    write(1, alphabet, 27);
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+write(1, alphabet, 27);
+
 }
