8b5c3e5b9313925ef704609c99aafbb0e4287005
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-23 17:13:01 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,13 +6,14 @@
  *
  *
  */
-void print_alphabet(void)
-{
-    char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        write(1, &letter, 1);
-    }
-    write(1, "\n", 1);
+void print_alphabet(void)
+
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+write(1, &letter, 1);
+}
+write(1, "\n", 1);
 }
