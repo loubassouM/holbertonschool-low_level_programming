392d2abcc1d1862676be483f3bcd3e3a4db37a7b
loubassouM <loubassoumurphy@gmail.com>
2025-02-24 12:05:03 +0000

    4-print_most_numbers.C

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,17 +3,18 @@
 /**
  * print_most_numbers - prints characters from 0 to 9, excluding 2 and 4
  */
+
 void print_most_numbers(void)
 {
 char c;
- 
+
 for (c = '0'; c <= '9'; c++)
 {
-if (c == '2' || C == '4')
+if (c == '2' || c == '4')
 {
 continue;
-_putchar(c);
 }
+_putchar(c);
 }
 _putchar('\n');
 }
