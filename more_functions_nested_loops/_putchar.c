d694aca7c0bd5db8f5065d300cedef0d1a0171e6
loubassouM <loubassoumurphy@gmail.com>
2025-02-24 18:03:01 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to standard output
- * @c: The character to print
- * Return: On success 1, On error -1 is returned
- */
+* _putchar - writes a character to standard output
+* @c: The character to print
+* Return: On success 1, On error -1 is returned
+*/
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+return write(1, &c, 1);
 }
