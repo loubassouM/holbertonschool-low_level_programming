7b7a299542feb84dad7211cbd7594af52d5ddaa7
loubassouM <loubassoumurphy@gmail.com>
2025-02-24 10:29:01 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+char c = '0';
+
+while (c <= '9')
+   {
+_putchar(c);
+c++;
+  }
+_putchar('\n');
+}
