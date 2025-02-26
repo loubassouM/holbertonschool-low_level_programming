f4b219855bb315629b5fcd2d79a8265697d48568
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 08:47:31 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
+int i; int j;
 char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
-int i;
 for (i = 0; i < 10; i++)
 {
-_putchar(*alphabet);
+for (j = 0; j < 26; j++)
+{
+_putchar(alphabet[j]);
+}
 _putchar('\n');
 }
 }
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
 
 /**
- *
- *
- *
- *
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: On success 1, On error -1
  */
-int _putchar(char c);
+int _putchar(char c)
 {
-    return write(1, &c, 1);
+return (write(1, &c, 1));
 }
