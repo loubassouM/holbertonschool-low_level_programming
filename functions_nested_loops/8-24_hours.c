a5c9e426beccb7f20c7ea85928aeac51899906e5
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 12:10:56 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 /**
-* jack_bauer - prints every minute of the day of Jack Bauer
-* starting from 00:00 to 23:59
-*/
+ * _putchar - writes a character to standard output
+ * @c: The character to write
+ * Return: On success, returns the number of bytes written.
+ */
+int _putchar(char c);
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
 int hour, minute;
@@ -11,7 +20,12 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-printf("%02d:%02d\n", hour, minute);
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
 }
 }
 }
