571d4eae9396a76b3495e4561cde645ed673fec7
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 12:17:30 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int i, j, product;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+product = i * j;
+if (j != 0)
+printf(", ");
+if (product < 10)
+printf(" ");
+printf("%d", product);
+}
+printf("\n");
+}
+}
