235165c9de1b5e39c548c34d507f76894ed4a7a3
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 08:23:50 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10
- */
+* print_alphabet_x10
+*/
 void print_alphabet_x10(void)
 {
 int i;
 char c;
-
 for (i = 0; i < 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
