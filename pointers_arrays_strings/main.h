70f50ab75b327107cd4de34ed1cec6550727c68d
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 14:26:47 +0000

    2-strlen.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,13 @@ void print_square(int size);
 void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _strlen(char *s);
 
 #endif
