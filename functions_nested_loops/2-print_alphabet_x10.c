72899f3ab599b99d86576ecf94fcdbcdc2fa9fa2
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-26 09:41:32 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 int i; int j;
-char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 26; j++)
 {
-_putchar(alphabet[j]);
+_putchar('a' + j);
 }
 _putchar('\n');
+}
 }
