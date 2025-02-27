53bd119a16ec7e8b5f514efabc0f1679670e45d1
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-27 23:39:15 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,19 +2,15 @@
 #include <unistd.h>
 
 /**
- *
- */
+*
+*/
 
 void print_alphabet(void)
 {
-
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-
-	write(1, &c, 1);
+write(1, &c, 1);
 }
-
-	write(1, "\n", 1);
+write(1, "\n", 1);
 }
