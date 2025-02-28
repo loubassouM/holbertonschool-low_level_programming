e033ba333765755b19b43946efd552f939d76177
«loubassouM» <loubassoumurphy@gmail.com>
2025-02-28 15:25:02 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,11 +2,11 @@
 #include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to convert.
- *
- * Return: The integer value, or 0 if no numbers are found.
- */
+* _atoi - Converts a string to an integer.
+* @s: The string to convert.
+*
+* Return: The integer value, or 0 if no numbers are found.
+*/
 int _atoi(char *s)
 {
 int i = 0, sign = 1, result = 0, found_digit = 0;
