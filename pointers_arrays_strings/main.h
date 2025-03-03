d74cfe64891c605d98aa953fa343beba0ab59bbf
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-03 14:14:32 +0000

    6-puts2.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,5 +25,13 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _strlen(char *s);
 void _puts(char *str);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
