a1e5bbd1fb9a0bfdd5c0e627febc1d6c33ff6641
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-03 16:06:36 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,22 +8,16 @@
  * (length_of_string + 1) / 2 characters. If the string length is even,
  * it prints the last length_of_string / 2 characters.
  */
-void puts_half(char *str)
-{
+void puts_half(char *str) {
 int len = 0;
 int start;
 int i;
-{
-while (str[len] != '\0')
+while (str[len] != '\0') {
 len++;
-{
-if (len % 2 == 0)
-start = len / 2;
-else
+}
 start = (len + 1) / 2;
+for (i = start; str[i] != '\0'; i++) {
+putchar(str[i]);
 }
-for (i = start; str[i] != '\0'; i++)
-write(1, &str[i], 1);
-write(1, "\n", 1);
-}
+putchar('\n');
 }
--- pointers_arrays_strings/_putchar.c
+++ pointers_arrays_strings/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
- * Return: On success 1, on error -1.
- */
+* _putchar - Writes a character to stdout.
+* @c: The character to print.
+* Return: On success 1, on error -1.
+*/
 int _putchar(char c)
 {
 return write(1, &c, 1);
