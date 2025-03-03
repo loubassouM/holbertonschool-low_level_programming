1b0ef3075b832ce8e578ef917d953589a2c26961
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-03 16:13:18 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,18 @@
  * (length_of_string + 1) / 2 characters. If the string length is even,
  * it prints the last length_of_string / 2 characters.
  */
-void puts_half(char *str) {
+void puts_half(char *str)
+{
 int len = 0;
 int start;
 int i;
-while (str[len] != '\0') {
+while (str[len] != '\0')
+{
 len++;
 }
 start = (len + 1) / 2;
-for (i = start; str[i] != '\0'; i++) {
+for (i = start; str[i] != '\0'; i++)
+{
 putchar(str[i]);
 }
 putchar('\n');
