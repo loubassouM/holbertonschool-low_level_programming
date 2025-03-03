45a4950ccc236e37d21bffb6399dd6455ca2de5f
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-03 15:35:16 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void puts_half(char *str)
+{
+int len = 0;
+int start;
+int i;
+{
+while (str[len] != '\0')
+len++;
+{
+if (len % 2 == 0)
+start = len / 2;
+else
+start = (len + 1) / 2;
+}
+for (i = start; str[i] != '\0'; i++)
+putchar(str[i]);
+putchar('\n');
+}
+}
