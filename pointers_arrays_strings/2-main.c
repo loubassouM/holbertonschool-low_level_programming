343d5e36ce30dd5849cfe2b6689dda31168a19e8
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-04 10:53:24 +0000

    2-strchr.c

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-char *str;
-int len;
-str = "My first strlen!";
-len = _strlen(str);
-printf("%d\n", len);
+char *s = "hello";
+char *f;
+f = _strchr(s, 'l');
+if (f != NULL)
+{
+printf("%s\n", f);
+}
 return (0);
 }
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to search
+ * @c: The character to locate
+ *
+ * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (c == '\0')
+return (s);
+return (NULL);
+}
