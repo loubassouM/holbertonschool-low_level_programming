f73a7997fa13d432c1a8cbb7f69e0c4a78a1b27d
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-06 12:53:39 +0000

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,1 +1,14 @@
- 
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Return: Length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
