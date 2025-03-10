54f3e343f83da6e3f9c5080022c61672a2c19bbc
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-10 11:04:26 +0000

    0-create_array.c

--- malloc_array.c/main.c
+++ malloc_array.c/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
+    char *arr = create_array(5, 'A');
+
+    if (arr == NULL)
+    {
+        printf("Failed to allocate memory\n");
+        return (1);
+    }
+
+    for (int i = 0; i < 5; i++)
+        printf("%c ", arr[i]);
+    printf("\n");
+
+    free(arr);
     return (0);
 }
