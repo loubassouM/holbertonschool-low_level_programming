acf1a0b101737f3cb2c15df215437166d5ff21f3
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-13 10:12:21 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,31 +13,22 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat_str;
-unsigned int i = 0, j = 0;
+unsigned int len1 = 0, len2 = 0, i, j;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-concat_str = malloc(strlen(s1) + strlen(s2) + 1);
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
+concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
 if (concat_str == NULL)
-{
 return (NULL);
-}
-while (s1[i] != '\0')
-{
+for (i = 0; i < len1; i++)
 concat_str[i] = s1[i];
-i++;
-}
-while (s2[j] != '\0')
-{
-concat_str[i] = s2[j];
-i++;
-j++;
-}
-concat_str[i] = '\0';
+for (j = 0; j < len2; j++)
+concat_str[i + j] = s2[j];
+concat_str[i + j] = '\0';
 return (concat_str);
 }
