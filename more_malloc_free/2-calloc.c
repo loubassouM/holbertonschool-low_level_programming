3f9ad14d923797c9906fd3456bc9ac059cd813f4
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-14 22:29:12 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,13 +10,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
+unsigned int total_size;
 if (nmemb == 0 || size == 0)
 return (NULL);
-if (nmemb && size > (size_t)-1 / nmemb)
-return (NULL);
-ptr = malloc(nmemb * size);
+total_size = nmemb * size;
+ptr = malloc(total_size);
 if (ptr == NULL)
 return (NULL);
-memset(ptr, 0, nmemb * size);
+memset(ptr, 0, total_size);
 return (ptr);
 }
