0c710e6066069d7d2001ebe089ca46a26713bb59
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-14 21:13:13 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,35 @@
- 
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates two strings with at most n bytes from s2
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes to copy from s2
+ * Return: Pointer to the newly allocated memory, or NULL if fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int len1 = 0, len2 = 0, i, j;
+char *new_str;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
+if (n >= len2)
+n = len2;
+new_str = malloc(len1 + n + 1);
+if (new_str == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+new_str[i] = s1[i];
+for (j = 0; j < n; j++)
+new_str[i + j] = s2[j];
+new_str[i + j] = '\0';
+return (new_str);
+}
