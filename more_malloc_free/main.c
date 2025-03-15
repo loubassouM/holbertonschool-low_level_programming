4c1db1789ab7e6a9e3398b5a209ad40f2527326f
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-15 09:25:16 +0000

    2-calloc.c

--- more_malloc_free/main.c
+++ more_malloc_free/main.c
@@ -4,18 +4,12 @@
 
 int main(void)
 {
-    char *arr = create_array(5, 'A');
-
-    if (arr == NULL)
-    {
-        printf("Failed to allocate memory\n");
-        return (1);
-    }
-
-    for (int i = 0; i < 5; i++)
-        printf("%c ", arr[i]);
-    printf("\n");
-
-    free(arr);
-    return (0);
+char *a;
+a = _calloc(98, sizeof(char));
+strcpy(a, "Best");
+strcpy(a + 4, " School! :)\n");
+a[97] = '!';
+simple_print_buffer(a, 98);
+free(a);
+return (0);
 }
