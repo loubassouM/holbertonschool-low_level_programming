722ee9532488c13e8c4838a8a1a5fbed125d8283
«loubassouM» <loubassoumurphy@gmail.com>
2025-03-16 18:36:43 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,21 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - Defines a new struct for a dog
- * @name: Name of the dog (string)
- * @age: Age of the dog (float)
- * @owner: Owner of the dog (string)
- */
+*struct dog - Defines a new struct for a dog
+*@name: Name of the dog (string)
+*@age: Age of the dog (float)
+*@owner: Owner of the dog (string)
+*/
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 } dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
