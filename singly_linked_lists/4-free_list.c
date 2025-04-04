f3338a0ac12f2e9b6293581f0a4612b5052f764a
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-04 11:23:44 -0700

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,18 @@
- 
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_list - Free a list_t linked list.
+*@head : Pointer to the head of the list.
+*
+*/
+void free_list(list_t *head)
+{
+list_t *temp;
+while (head)
+temp = head ->next;
+free (head->str);
+free (head);
+head = temp;
+}
+}
