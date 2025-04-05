eae5546c20efc45d3f7b0eeec3ef10a1d99cb1ac
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-05 00:09:48 -0700

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,45 +1,38 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
- * @head: A pointer to the head of the list.
- * @index: The index of the node to delete.
- *
- * Return: 1 if it succeeded, -1 if it failed.
- */
+* delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
+* @head: A pointer to the head of the list.
+* @index: The index of the node to delete.
+*
+* Return: 1 if it succeeded, -1 if it failed.
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *temp = *head;
 unsigned int i = 0;
-if (*head == NULL)  
+if (*head == NULL)
 return (-1);
 if (index == 0)
 {
 *head = temp->next;
 if (*head)
 (*head)->prev = NULL;
-free(temp); 
+free(temp);
 return (1);
-    }
-
-    /* Traverse the list to find the node at the given index */
-    while (temp && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
-
-    /* If the node doesn't exist at the given index */
-    if (temp == NULL)
-        return (-1);
-
-    /* Adjust the previous and next nodes to unlink the current node */
-    if (temp->next)
-        temp->next->prev = temp->prev;
-    if (temp->prev)
-        temp->prev->next = temp->next;
-
-    free(temp);  /* Free the node */
-    return (1);
 }
-
+while (temp && i < index)
+{
+temp = temp->next;
+i++;
+}
+if (temp == NULL)
+return (-1);
+if (temp->next)
+temp->next->prev = temp->prev;
+if (temp->prev)
+temp->prev->next = temp->next;
+free(temp);
+return (1);
+}
