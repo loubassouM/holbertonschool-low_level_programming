0abf4c112ed4adc0404cf25abac1fc2eb9706fac
«loubassouM» <loubassoumurphy@gmail.com>
2025-04-08 00:04:09 +0000

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,1 +1,21 @@
- 
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary - Prints the binary representation of a number
+* @n: Unsigned long integer to convert to binary
+*
+*/
+void print_binary(unsigned long int n)
+{
+unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+while (mask > 0 && (n & mask) == 0)
+mask >>= 1;
+if (mask == 0)
+putchar('0');
+while (mask > 0)
+{
+putchar((n & mask) ? '1' : '0');
+mask >>= 1;
+}
+}
